f4b5945aed4f7f464ba2eafd8dc9efb141c8eb4d
shamela <shamelashahnas@gmail.com>
2024-04-24 11:55:27 +0530

    addedsff

--- Ehsaas/settings.py
+++ Ehsaas/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-@a&o-@1l4gx5&bioqf*7(!1gh-w2l#r(ooint_keoh(rkudwt6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
